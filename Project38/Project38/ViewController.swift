3e5c2412ac4f3430e7599826f5f0668ce5729ee7
Andrew Lundy <andrewlundy@Andrews-iMac.local>
2019-08-17 20:40:56 -0500

    Add all projects

---
+++ Project38/Project38/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project38
+//
+//  Created by Andrew Lundy on 8/16/19.
+//  Copyright © 2019 Andrew Lundy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
