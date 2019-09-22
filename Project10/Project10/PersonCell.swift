9d9ef7e74426d415e71fd19221fbd435d9edf69a
Andrew Lundy <andrewlundy@Andrews-iMac.local>
2019-09-22 11:31:47 -0500

    Add and finish Project10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Andrew Lundy on 9/21/19.
+//  Copyright © 2019 Andrew Lundy. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
