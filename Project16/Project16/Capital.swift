91d44ddfd3973f0a98ed13819b5d1e43df4a930e
Andrew Lundy <andrewlundy@Andrews-iMac.local>
2019-11-01 18:53:46 -0500

    Finish Project16

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,24 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Andrew Lundy on 10/31/19.
+//  Copyright © 2019 Andrew Lundy. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+}
