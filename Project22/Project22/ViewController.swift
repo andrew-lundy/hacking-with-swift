c7bcdb403f82989558834bd77718090ef497b2e0
Andrew Lundy <andrewlundy@Andrews-iMac.local>
2019-11-20 20:47:47 -0600

    Start Project22

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Andrew Lundy on 11/20/19.
+//  Copyright © 2019 Andrew Lundy. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    var locationManager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
+
+
+}
+
