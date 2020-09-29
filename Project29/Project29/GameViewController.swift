cfa16851be77d4ae6f2b0bb0ff58baf1712e74a3
Andrew Lundy <andrewlundy@icloud.com>
2020-09-28 19:31:24 -0500

    Start Project 29

--- Project29/Project29/GameViewController.swift
+++ Project29/Project29/GameViewController.swift
@@ -10,7 +10,32 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var currentGame: GameScene!
+    
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var angleLabel: UILabel!
+    @IBOutlet weak var velocitySlider: UISlider!
+    @IBOutlet weak var velocityLabel: UILabel!
+    @IBOutlet weak var launchButton: UIButton!
+    @IBOutlet weak var playerNumber: UILabel!
+    
+    // MARK: - Methods
+    @IBAction func angleChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func velocityChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func launch(_ sender: Any) {
+        
+    }
+    
+    
+    // MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +47,8 @@ class GameViewController: UIViewController {
                 
                 // Present the scene
                 view.presentScene(scene)
+                currentGame = scene as? GameScene
+                currentGame.viewController = self
             }
             
             view.ignoresSiblingOrder = true
