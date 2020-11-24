cd2ff696bffa7fea71db777b3a21c23cc45350b2
Andrew Lundy <andrewlundy@icloud.com>
2020-11-23 20:35:09 -0600

    Finish: How to make a Core Data attribute unique using constraints

--- Project38/Project38/ViewController.swift
+++ Project38/Project38/ViewController.swift
@@ -12,19 +12,40 @@ import CoreData
 class ViewController: UITableViewController {
 
     var container: NSPersistentContainer!
+    var commits = [Commit]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         container = NSPersistentContainer(name: "Project38")
         container.loadPersistentStores { (storeDescription: NSPersistentStoreDescription, error: Error?) in
+            self.container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+            
             if let error = error {
                 print("Unresolved error \(error)")
             }
         }
         
         performSelector(inBackground: #selector(fetchCommits), with: nil)
+        loadSavedData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commits.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
+
+        let commit = commits[indexPath.row]
+        cell.textLabel!.text = commit.message
+        cell.detailTextLabel!.text = commit.date.description
+
+        return cell
     }
     
     func configure(commit: Commit, usingJSON json: JSON) {
@@ -47,6 +68,20 @@ class ViewController: UITableViewController {
         }
     }
     
+    func loadSavedData() {
+        let request = Commit.createFetchRequest()
+        let sort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [sort]
+        
+        do {
+            commits = try container.viewContext.fetch(request)
+            print("Got \(commits.count) commits")
+            tableView.reloadData()
+        } catch {
+            print("FETCH FAILED")
+        }
+    }
+    
     @objc func fetchCommits() {
         if let data = try? String(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!) {
             let jsonCommits = JSON(parseJSON: data)
@@ -61,6 +96,7 @@ class ViewController: UITableViewController {
                     self.configure(commit: commit, usingJSON: jsonCommit)
                 }
                 self.saveContext()
+                self.loadSavedData()
             }
             
         }
