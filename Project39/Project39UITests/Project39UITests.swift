f5da6c00e2a063d31926562e0b809da747d61a94
Andrew Lundy <andrewlundy@icloud.com>
2020-12-21 18:16:47 -0600

    Added test that verifies there are 55 table rows when the user filters by words that appear 1000 or more times.

--- Project39/Project39UITests/Project39UITests.swift
+++ Project39/Project39UITests/Project39UITests.swift
@@ -38,6 +38,19 @@ class Project39UITests: XCTestCase {
         
     }
     
+    func testUserFilterBy1000() {
+        let app = XCUIApplication()
+        app.launch()
+        app.buttons["Search"].tap()
+        
+        let filterAlert = app.alerts
+        let textField = filterAlert.textFields.element
+        textField.typeText("\(1000)")
+        
+        filterAlert.buttons["Filter"].tap()
+        XCTAssertEqual(app.tables.cells.count, 55, "There should be 55 words matching that are seen 1000+ times")
+    }
+    
     func testInitialStateIsCorrect() {
         let app = XCUIApplication()
         app.launch()
